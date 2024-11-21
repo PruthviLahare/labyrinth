e695729d7e5c5f8bc1cb02f80a241aa766f2328d
PruthviLahare <142437821+PruthviLahare@users.noreply.github.com>
2024-11-21 18:15:32 +0530

    Update labyrinth.js

--- labyrinth.js
+++ labyrinth.js
@@ -1,1 +1,19 @@
+function isBombPresent(position, bombPosition) {
+  let currentBombPos = '';
 
+  for (let index = 0; index < bombPosition.length; index++) {
+    if (bombPosition[index] === ' ') {
+      currentBombPos = '';
+    }
+
+    currentBombPos += bombPosition[index];
+
+    if (+currentBombPos === position) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+console.log(isBombPresent(20, '20 90 30 10'));
